ac96fd1a69b91207ca593a8956caf156b0546ac4
francisco souza <108725+fsouza@users.noreply.github.com>
2022-10-10 17:55:29 -0400

    brew is no longer available in Linux builders
    Also fix the script

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
-tag=$(git describe --tags HEAD)
-old_sha=$(shasum -a 256 README.md)
-sd 'rev: .+' "rev: ${tag}" README.md
-new_sha=$(shasum -a 256 README.md)
+if git describe --tags --exact-match HEAD &>/dev/null; then
+	tag=$(git describe --tags --exact-match HEAD)
+	old_sha=$(shasum -a 256 README.md)
+	sed -i 's/rev: .\+/rev: '"${tag}" README.md
+	new_sha=$(shasum -a 256 README.md)
 
-if [[ ${new_sha} != "${old_sha}" ]]; then
-	git commit -m "Update README" README.md
+	if [[ ${new_sha} != "${old_sha}" ]]; then
+		git commit -m "Update README" README.md
+	fi
 fi
