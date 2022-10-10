50cb523dc101bbc77af3ca24701785293783be46
francisco souza <108725+fsouza@users.noreply.github.com>
2022-10-10 17:48:34 -0400

    scripts/update-readme: commit

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -3,4 +3,10 @@
 set -euo pipefail
 
 tag=$(git describe --tags HEAD)
+old_sha=$(shasum -a 256 README.md)
 sd 'rev: .+' "rev: ${tag}" README.md
+new_sha=$(shasum -a 256 README.md)
+
+if [[ ${new_sha} != "${old_sha}" ]]; then
+	git commit -m "Update README" README.md
+fi
