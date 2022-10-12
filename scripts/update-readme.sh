ddbed71c5c8890692f408d2a8f5a7571b93066c9
francisco souza <108725+fsouza@users.noreply.github.com>
2022-10-12 09:40:00 -0400

    scripts/update-readme: fix sed expression

--- scripts/update-readme.sh
+++ scripts/update-readme.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 if git describe --tags --exact-match HEAD &>/dev/null; then
 	tag=$(git describe --tags --exact-match HEAD)
 	old_sha=$(shasum -a 256 README.md)
-	sed -i 's/rev: .\+/rev: '"${tag}" README.md
+	sed -i 's/rev: .\+/rev: '"${tag}/" README.md
 	new_sha=$(shasum -a 256 README.md)
 
 	if [[ ${new_sha} != "${old_sha}" ]]; then
